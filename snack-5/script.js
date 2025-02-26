c28f6f625c31b62c354a1e203c5beff2fce9065f
Pierpaolo-99 <pierpaolodellioio@gmail.com>
2025-02-26 13:50:35 +0100

    snack 5 complete

--- snack-5/script.js
+++ snack-5/script.js
@@ -2,3 +2,19 @@ const nums = [2, 8, 4, 7, 12, 87];
 
 // Crea un array a partire dall'array nums, che abbia solo i numeri pari
 // Risultato: [2, 8, 4, 12]
+
+// exstended
+const filteredEven = nums.filter((number) => {
+    if (number % 2 === 0){
+        return true
+    }
+    return false
+})
+
+console.log(filteredEven);
+
+// abbreviated
+const filteredEvenNumbers = nums.filter( number => number % 2 === 0)
+
+console.log(filteredEvenNumbers);
+
